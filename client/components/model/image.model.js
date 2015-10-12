589f425d4c8122582daa7e66695a3bd6cee9fb31
skullacy <skullacy@gmail.com>
2015-10-12 15:13:09 +0900

    oImage 구현완료

--- client/components/model/image.model.js
+++ client/components/model/image.model.js
@@ -17,7 +17,7 @@ angular.module('mydearnest')
 		//RestangularProvider.addElementTransformer('magazines', false, function(element) {
 		//});
 	}])
-	.factory("oImage", function() {
+	.factory("oImage",['MdnConfig', function(MdnConfig) {
 
 		/**
 		 * Constructor
@@ -59,7 +59,97 @@ angular.module('mydearnest')
 			 * @restriction not null, not 0
 			 */
 			this.height = data.height;
+
+			/**
+			 * @ngdoc property
+			 * @name resizeWidth
+			 * @type {number}
+			 * @propertyOf oImage
+			 * @description
+			 * `API명세에 없는 property`
+			 * 리사이즈 이미지 width
+			 *
+			 * getImageURL()을 통하여 이미지 url을 받을때 리사이즈 속성이 세팅된 값이 반영된다.
+			 * `Default : 0`
+			 */
+			this.resizeWidth = 0;
+
+			/**
+			 * @ngdoc property
+			 * @name resizeHeight
+			 * @type {number}
+			 * @propertyOf oImage
+			 * @description
+			 * `API명세에 없는 property`
+			 * 리사이즈 이미지 height
+			 *
+			 * getImageURL()을 통하여 이미지 url을 받을때 리사이즈 속성이 세팅된 값이 반영된다.
+			 * `Default : 0`
+			 */
+			this.resizeHeight = 0;
 		}
+
+		/**
+		 * Public method
+		 * 프로토타입으로 선언
+		 */
+
+		/**
+		 * @ngdoc method
+		 * @name getImageURL
+		 * @methodOf oImage
+		 * @returns {string} 이미지 URL (프로토콜 제외)
+		 * @description
+		 * 해당 이미지를 불러오기위한 url을 호출.
+		 */
+		oImage.prototype.getImageURL = function() {
+			var resizeString = '';
+			if(this.resizeWidth > 0 || this.resizeHeight > 0) {
+				resizeString = resizeWidth + 'x' + resizeHeight;
+			}
+			return MdnConfig.IMAGE_URL + '/unsafe' + resizeString + '/' + this.img_id + '/' + this.img_id;
+		};
+
+		/**
+		 * @ngdoc method
+		 * @name setResizeWidth
+		 * @methodOf oImage
+		 * @param {number} width 이미지 가로
+		 * @returns {oImage}
+		 * @description
+		 * getImageURL() 호출전 해당 메소드를 이용하여 리사이징될 이미지를 세팅할 수 있다.
+		 *
+		 * 자기자신을 리턴하므로 체인으로 연결하여 사용하면 편함
+		 * <pre>
+		 * image.setResizeWidth(100).getImageURL();
+		 * image.setResizeWidth(150).setResizeHeight(150).getImageURL();
+		 * </pre>
+		 */
+		oImage.prototype.setResizeWidth = function(width) {
+			if(width >= 0) this.resizeWidth = width;
+			return this;
+		}
+
+		/**
+		 * @ngdoc method
+		 * @name setResizeHeight
+		 * @methodOf oImage
+		 * @param {number} height 이미지 가로
+		 * @returns {oImage}
+		 * @description
+		 * getImageURL() 호출전 해당 메소드를 이용하여 리사이징될 이미지를 세팅할 수 있다.
+		 *
+		 * 자기자신을 리턴하므로 체인으로 연결하여 사용하면 편함
+		 * <pre>
+		 * image.setResizeWidth(100).getImageURL();
+		 * image.setResizeWidth(150).setResizeHeight(150).getImageURL();
+		 * </pre>
+		 */
+		oImage.prototype.setResizeHeight = function(height) {
+			if(height >= 0) this.resizeHeight = height;
+			return this;
+		}
+
 
 		/**
 		 * Static methods
@@ -67,10 +157,10 @@ angular.module('mydearnest')
 		 */
 		oImage.build = function(data) {
 			return new oImage(data);
-		}
+		};
 
 		/**
 		 * oImage Constructor 리턴
 		 */
 		return oImage;
-	});
+	}]);
